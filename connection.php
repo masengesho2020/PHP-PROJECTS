1c36145275f2be2c1873390f0c736ee71651b89c
masengesho2020 <59082204+masengesho2020@users.noreply.github.com>
2026-05-21 14:57:13 +0200

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+$connection=mysqli_connect("185.27.134.116","if0_41984352 ","8jKXEyBwPJjC","if0_41984352_schooldb");
+
+if($connection){
+    echo "Database connected successful";
+}
+else {
+    die "Database not connected"
+}
+
+?>
